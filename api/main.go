a2dc58848864262118dede23e03679a71361267e
Albert Mulia Shintra <chenxeed@gmail.com>
2022-10-09 22:42:04 +0700

    feat(api): add router to add new task

--- api/main.go
+++ api/main.go
@@ -15,6 +15,7 @@ const (
 )
 
 type task struct {
+	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"desc"`
 	DueDate     int    `json:"dueDate"`
@@ -23,8 +24,8 @@ type task struct {
 
 // TODO: Get the data via PostgreSQL
 var tasks = []task{
-	{Name: "Open email", Description: "Find the new email that sent from our newest client", DueDate: 1675513005240, CreateDate: 1665213005240},
-	{Name: "Read the book", Description: "You bought a new book. Now read it.", DueDate: 1675514005240, CreateDate: 1665213005240},
+	{Id: "1", Name: "Open email", Description: "Find the new email that sent from our newest client", DueDate: 1675513005240, CreateDate: 1665213005240},
+	{Id: "2", Name: "Read the book", Description: "You bought a new book. Now read it.", DueDate: 1675514005240, CreateDate: 1665213005240},
 }
 
 // getTask responds with the list of all tasks as JSON.
@@ -32,9 +33,24 @@ func getTasks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+func postTask(c *gin.Context) {
+	var newTask task
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/tasks", getTasks)
+	router.POST("/task", postTask)
 	// TODO: Determine the root URL via .env for cross-environment
 	router.Run("localhost:5050")
 }
