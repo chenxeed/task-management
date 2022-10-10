997ff64bd29b3dddaa290685b9b4b1ddacb65e45
Albert Mulia Shintra <chenxeed@gmail.com>
2022-10-10 22:12:08 +0700

    chore: add basic styling - install tailwind - init basic component for task

---
+++ webapp/src/components/TaskList/index.tsx
@@ -0,0 +1,14 @@
+import { Task } from '../../typings/task'
+
+function TaskList(props: { task: Task }) {
+
+  return (
+    <div className="p-10 text-left border">
+      <div className="text-lg border border-b-2 border-t-0 border-x-0">{ props.task.name }</div>
+      <div className="text-md">{ props.task.desc }</div>
+      <div className="text-sm text-right text-gray-500">{ props.task.createdDate }</div>
+    </div>
+  )
+}
+
+export default TaskList
