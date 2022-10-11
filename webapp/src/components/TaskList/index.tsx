0f3c15b52ea0fdfecf30f6dac3e726eed216a4a5
Albert Mulia Shintra <chenxeed@gmail.com>
2022-10-12 01:33:48 +0700

    feat: show status

--- webapp/src/components/TaskList/index.tsx
+++ webapp/src/components/TaskList/index.tsx
@@ -3,11 +3,22 @@ import dayjs from 'dayjs'
 
 function TaskList(props: { task: Task }) {
 
+  const getStatus = (dueDate: number) => {
+    const dueDateToNow = dayjs(dueDate).diff(dayjs().valueOf(), 'day')
+    if (dueDateToNow > 7) {
+      return 'Not urgent'
+    } else if (dueDateToNow < 0) {
+      return 'Overdue'
+    } else {
+      return 'Due soon'
+    }
+  }
+
   return (
     <div className="p-10 text-left border">
       <div className="border border-b-2 border-t-0 border-x-0 flex justify-between">
         <div className='text-lg'>{ props.task.name }</div>
-        <span className='text-sm'>Due: { dayjs(props.task.dueDate).format('DD-MM-YYYY') }</span>
+        <span className='text-sm'>Due: { dayjs(props.task.dueDate).format('DD-MM-YYYY') } ({getStatus(props.task.dueDate)})</span>
       </div>
       <div className="text-md">{ props.task.desc }</div>
       <div className="text-sm text-right text-gray-500">{ dayjs(props.task.createdDate).format() }</div>
